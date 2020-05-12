cd941909bd6e4e82cb88483b774f8e8712d68ae5
mustafa abdishakur <mx_1200@hotmail.com>
2020-05-12 16:38:43 +0400

    commit #2

--- contact.js
+++ contact.js
@@ -1,15 +1,7 @@
 
 const contactList = document.querySelectorAll('li');
-const headerList = document.querySelectorAll('ul');
-let contactArr = [];
-
-headerList.forEach(contact => {
-    console.log(contact);
-
-});
 document.querySelector('input').addEventListener('input', el => {
     const input = el.target.value;
-
     contactList.forEach(contact => {
         let contactName = contact.innerHTML.toLowerCase();
         if (!(contactName.includes(`${input}`))) {
@@ -20,8 +12,3 @@ document.querySelector('input').addEventListener('input', el => {
     });
     
 });
-/*
-*get input
- * compare input with contact list
- * display contact if input is similar
- */
